c6470518df5e8698ffd5c4d8f2d3540cce98ba81
Rajas Chitale <56029517+rajas1310@users.noreply.github.com>
2022-05-15 23:39:51 +0530

    Update Assign4.py

--- Assign4.py
+++ Assign4.py
@@ -48,67 +48,8 @@ if stat == True:
 
 else:
     print("No result possible")
+
 #____________________
-
-# Branch and Bound
-
-def isSafe(board, row, col):
-    n_rows = len(board)
-    n_cols = len(board[0])
-    for i in range(n_rows):
-        if board[i][col] == 1:
-            return False
-            
-    for j in range(n_cols):
-        if board[row][j] == 1:
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
- 
-    # Check lower diagonal on left side
-    for i, j in zip(range(row, n_rows, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
- 
-    return True
-
-def printSolution(board):
-    n_rows = len(board)
-    n_cols = len(board[0])
-    for i in range(n_rows):
-        for j in range(n_cols):
-            print (board[i][j], end=" ")
-        print()
-
-def solve(board, col):
-    n_rows = len(board)
-    n_cols = len(board[0])
-    for i in range(n_rows):
-        if col>=n_cols:
-            return True
-        if isSafe(board, i, col):
-            board[i][col] = 1
-
-            if solve(board, col+1):
-                #print(col)
-                return True
-            
-            board[i][col] = 0
-    return False
-
-board = []
-for i in range(8):
-    board.append([])
-    for j in range(8):
-        board[i].append(0)
-
-if solve(board, 0) == False:
-    print("Solution doesnt exist")
-else:
-    printSolution(board)
-
 
 #BRANCH AND BOUND
 
